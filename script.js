1970345ceff64fda8a9cdf8a543d9d1dd4e7ea61
yc0828 <57612163+yc0828@users.noreply.github.com>
2019-12-23 14:16:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // file: script.js
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyAvHToVDiX6-8vxSml0rg9S80hIxjoEojw",
-    authDomain: "dec222019.firebaseapp.com",
-    databaseURL: "https://dec222019.firebaseio.com",
-    projectId: "dec222019",
-    storageBucket: "dec222019.appspot.com",
-    messagingSenderId: "592439968262"
+    apiKey: "AIzaSyDE1jD65SvFx2TwHAe-0zhpKogtQf9pmW0",
+    authDomain: "project-2931774542081854471.firebaseapp.com",
+    databaseURL: "https://project-2931774542081854471.firebaseio.com",
+    projectId: "project-2931774542081854471",
+    storageBucket: "project-2931774542081854471.appspot.com",
+    messagingSenderId: "485556393277"
 };
 firebase.initializeApp(config);
 
